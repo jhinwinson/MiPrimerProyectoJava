5e53262942a4d32e82e15aab79ad888266670fae
jhinwinson <josorioc@ufpso.edu.co>
2025-04-08 07:23:35 -0500

    ejercicio1

--- MiPrimerProyectoJava/src/Practica2/Ejercicio1.java
+++ MiPrimerProyectoJava/src/Practica2/Ejercicio1.java
@@ -1,7 +1,11 @@
+/*Realizar un programa que defina un vector llamado “vector_numeros” de 10 enteros,
+ a continuación lo inicialice con valores aleatorios (del 1 al 10) 
+ y posteriormente muestre en pantalla cada elemento del vector junto
+  con su cuadrado y su cubo. */
 package Practica2;
 
-public class Ejercicio1 {
-    public static void main(String[] args) {
+public class Ejercicio1 {  // recuerda las varibles estilo camel case no con _
+     public static void main(String[] args) {
         
 
 
