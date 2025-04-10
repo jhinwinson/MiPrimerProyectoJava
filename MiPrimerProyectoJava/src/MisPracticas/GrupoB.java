3653757d1433479dfe98422f3f295a5d8d2db445
jhinwinson <josorioc@ufpso.edu.co>
2025-04-10 11:47:53 -0500

    Practica

--- MiPrimerProyectoJava/src/MisPracticas/GrupoB.java
+++ MiPrimerProyectoJava/src/MisPracticas/GrupoB.java
@@ -2,42 +2,51 @@ import java.util.Scanner;
 public class GrupoB {
     public static void main(String[] args) {
         Scanner leer = new Scanner(System.in);
+        final int aprobacion = 3;
+
         System.out.print("Ingrese la cantidad de estudiantes :");
         int cantidad = leer.nextInt(); //columnas
-        leer.nextLine();
+        leer.nextLine(); 
+
         String nombres[]= new String[cantidad];
-        int codigo[] = new int [cantidad];
-        int notas[][] = new int[3][4];               // quiero que con cada nuevo estudiante la matriz de notas cresca en 4 filas
+        int codigos[] = new int [cantidad];
+        double notas[][] = new double[cantidad][3];     //3 es el numero de asignaturas          
+        double[]promedios = new double[cantidad];
 
         for(byte i=0;i<cantidad;i++){
-            System.out.println("Ingrese el nombre del estudiante :");
-            String n = leer.nextLine();
-            nombres[i]=n;
-            System.out.println("Ingrese el codigo del estudiante");
-            int cod = leer.nextInt();
-            codigo[i] = cod;
+            System.out.print("Ingrese el nombre del estudiante :");
+            nombres[i]= leer.nextLine();
 
-            for(byte f = 0; f < 3; f++){ // se esta rellenando cada columna primero
-                for(byte c = 0; c < 4; c++){
-                    switch (c) {
-                        case 1:
-                        while (condition) {
-                            System.out.println("Ingrese las notas de Matematicas");
-                            int nota = leer.nextInt();
-                            notas[f][c] = nota;
-                        }
-                            break;
-                        case 2:
-                        System.out.println("Ingrese las notas de quimica");
-                        nota = leer.nextInt();
-                        notas[f][c] = nota;
-                    }
-                
+            System.out.print("Ingrese el codigo del estudiante");
+            codigos[i] = leer.nextInt();
+            
+            System.out.print("Ingrese la nota de Matemáticas: ");
+            notas[i][0] = leer.nextDouble();
 
+            System.out.print("Ingrese la nota de Química: ");
+            notas[i][1] = leer.nextDouble();
+
+            System.out.print("Ingrese la nota de Física: ");
+            notas[i][2] = leer.nextDouble();
+
+            promedios[i] = (notas[i][0] + notas[i][1] + notas[i][2])/3; //guarda el promedio en el primer recorrido
             }
 
+            System.out.println("--- REPORTE FINAL DEL CURSO ---");
 
+            for (int i = 0; i < cantidad; i++) {
+
+                System.out.println("Estudiante: " + nombres[i] + " (con el codigo: " + codigos[i] + ")");
+
+                System.out.printf("Tiene un promedio de ", promedios[i]);
+                if (promedios[i] >= aprobacion ){
+                    System.out.println("APROBADO");
+                } else {
+                    System.out.println("REPROBADO");
+                }
+      
+            }
         }
 
     }
-}
+
