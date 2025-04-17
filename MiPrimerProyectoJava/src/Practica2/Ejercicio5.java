dd76f4ec94859a09eef3c1970bd8144fec60046d
jhinwinson <josorioc@ufpso.edu.co>
2025-04-17 16:21:22 -0500

    Ejercicios

--- MiPrimerProyectoJava/src/Practica2/Ejercicio5.java
+++ MiPrimerProyectoJava/src/Practica2/Ejercicio5.java
@@ -1,5 +1,27 @@
+/* Hacer un programa que inicialice un vector de números con valores aleatorios,
+ y posterior ordene los elementos de menor a mayor.*/
 package Practica2;
-
+import java.util.Arrays;
+import java.util.Random;
 public class Ejercicio5 {
-    
+    public static void main(String[] args) {
+        Random numerosRandom = new Random();
+        double vector []= new double[10];
+        for(int i = 0; i<10 ;i++){
+            vector[i]=numerosRandom.nextInt(20);
+        }
+
+        System.out.println("---------------");
+        System.out.println("vector original");
+        for(int i = 0; i<10 ;i++){
+            System.out.println((int)vector[i]);
+        }
+        System.out.println("---------------");
+        
+          Arrays.sort(vector);//ordena el array de menor a mayor, 
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println((int)vector[i]);
+        }
+    }
 }
